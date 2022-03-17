792a7cf6a6170438b21bf98c5d3513f79fa077a3
yuwang <florawang.smile@gmail.com>
2022-03-16 18:43:08 -0700

    updated Contact view css

--- src/Contact.js
+++ src/Contact.js
@@ -5,7 +5,7 @@ function Contact() {
         <div className="contact">
             <span className="contact-us">Need help? Please contact us!</span>
             <div className="contact-method">
-                <span className="contact-email">Email: find-your-lost-pet@gmail.com</span><br/>
+                <span className="contact-email">Email: find-your-lost-pet@gmail.com</span><br />
                 <span className="contact-phone">Phone: +1-(666)-777-8886</span>
             </div>
         </div>
