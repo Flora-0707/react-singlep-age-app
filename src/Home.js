64974d5a84d0650468dd8e1400f4e7df7765ef00
yuwang <florawang.smile@gmail.com>
2022-03-16 18:33:35 -0700

    updated homepage css

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,9 @@ import cat from './cat_homepage.jpg';
 function Home() {
     return (
         <div className="homepage">
-            <span className='homepage-wel'>Welcome!</span><br/><br/>
-            Don't give up hope! Some animals go missing for weeks, sometimes even months, before they are found by their guardians.<br/>
-            <img src={cat} alt="cat"/>
+            <span className='homepage-wel'>Welcome!</span><br /><br />
+            <div className="homepage-message"> - Don't give up hope! Some animals go missing for weeks, sometimes even months, before they are found by their guardians. - </div>
+            <img src={cat} alt="cat" />
         </div>
     );
 }
